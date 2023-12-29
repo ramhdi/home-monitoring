16198910fef3ed2bd90c3231a42661c4b967fc4f
Rama Rahardi <rama.orion@gmail.com>
2023-12-29 14:32:38 +0700

    Remove unnecessary files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,6 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-  </header> -->
   <!-- <nav>
     <RouterLink to="/vue">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
